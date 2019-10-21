22c00cdfd407433d8ac7979a78d42456c8397c18
luckymichaln <michalnowak.contact@gmail.com>
2019-10-21 15:51:33 +0200

    add downloading Html as an image function

--- src/components/columns/ColumnCenter.js
+++ src/components/columns/ColumnCenter.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import DropZone from '../DropZone';
+import domtoimage from 'dom-to-image';
 
-const ColumnCenter = ({ backgroundImageSrc, nodes, moveNode, deleteNode, getNodeDimentions, dropZoneActive, showDeleteBtn }) => {
+const ColumnCenter = ({ backgroundImageSrc, nodes, moveNode, deleteNode, getNodeDimentions, dropZoneNode, dropZoneActive }) => {
 
   return (
     <div className="ColumnCenter">
@@ -10,12 +11,21 @@ const ColumnCenter = ({ backgroundImageSrc, nodes, moveNode, deleteNode, getNode
         nodes={nodes}
         moveNode={moveNode}
         deleteNode={deleteNode}
-        showDeleteBtn={showDeleteBtn}
         getNodeDimentions={getNodeDimentions}
         dropZoneActive={dropZoneActive}
         backgroundImageSrc={backgroundImageSrc}
       />
-      <button className="btn">Download as image</button>
+      <button className="btn" onClick={() => {
+        domtoimage.toJpeg(dropZoneNode, { quality: 1 })
+          .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = 'my-advert.jpeg';
+            link.href = dataUrl;
+            link.click();
+          });
+      }}>
+        Download as image
+      </button>
     </div>
   )
 }
